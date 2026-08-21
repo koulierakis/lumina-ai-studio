0704bb03ea12b0938e228bbc0c3cc23b2e8ca3d9
koulierakis <koulierakis@jsaglobal.gr>
2026-08-21 22:00:57 +0300

    chore(drive): cache pedestrian routing support

--- drive-assistant/sw.js
+++ drive-assistant/sw.js
@@ -1,5 +1,5 @@
-const CACHE='lumina-drive-v5';
-const CORE=['./','./index.html','./styles.css','./app-v2.js','./manifest.webmanifest'];
+const CACHE='lumina-drive-v6';
+const CORE=['./','./index.html','./styles.css','./app-v2.js','./walking-router.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
