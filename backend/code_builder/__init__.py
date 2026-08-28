0b65e15015d7ac9f049ad78f1b104e659edf6c23
koulierakis <koulierakis@jsaglobal.gr>
2026-08-28 20:13:09 +0300

    feat(code-builder): wire AI patch generation into task pipeline

--- backend/code_builder/__init__.py
+++ backend/code_builder/__init__.py
@@ -14,14 +14,17 @@ Modules include:
 - ollama_service
 - validation_service
 - patch_service
+- patch_generation_service
 - build_service
 - history_service
 - task_service
 - persistent_task_store
 """
 
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 
 from .persistent_task_store import install_persistent_task_store
+from .patch_generation_service import install_ai_patch_generation
 
 install_persistent_task_store()
+install_ai_patch_generation()
